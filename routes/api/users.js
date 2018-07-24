ed2e19f56dbcba66aae9b282c6b14a928a11a1b2
Angela Maugey <a.maugey@gmail.com>
2018-07-24 14:59:55 +0100

    Correcting typo

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,7 +28,9 @@ router.post("/register", (req, res) => {
 
 	User.findOne({ email: req.body.email }).then(user => {
 		if (user) {
-			return res.status(400).json({ email: "Email has already been register" })
+			return res
+				.status(400)
+				.json({ email: "Email has already been registered" })
 		} else {
 			const avatar = gravatar.url(req.body.email, {
 				s: 200,
